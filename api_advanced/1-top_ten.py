b6f7592a1fd56e8ee061ef63c8c7542d4dcf3c23
primitivecodes <p.ufitinema@alustudent.com>
2025-07-14 17:16:00 +0000

    hjui

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -3,17 +3,18 @@
 
 import requests
 
-
 def top_ten(subreddit):
     """Main function"""
-    URL = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    URL = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
-    
+
     try:
-        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
-        if RESPONSE.status_code == 200:
-            HOT_POSTS = RESPONSE.json().get("data").get("children")
-            [print(post.get('data').get('title')) for post in HOT_POSTS]
-        print("OK")  # Always print OK at the end
+        response = requests.get(URL, headers=HEADERS, allow_redirects=False)
+        if response.status_code == 200:
+            hot_posts = response.json().get("data", {}).get("children", [])
+            for post in hot_posts:
+                print(post.get('data', {}).get('title'))
     except Exception:
-        print("OK")  # Even on failure, print OK
+        pass
+
+    print("OK")
